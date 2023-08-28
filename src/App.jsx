60b0288fefd14f570808ac06424a02ae1e74dda2
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-08-28 17:45:14 +0530

    made no-unused var produce warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 // import Square from './Components/Square/';
 import Board from './Components/Board';
 // import './App.css';
 import './styles.scss';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  // let counter = 1;
+  console.log('hello');
+
+  const onBtnClick = () => {
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
+
   return (
     // <div className="card">
     //   {<h1>Title</h1>
@@ -15,8 +27,27 @@ function App() {
     //   </Square> }
     // </div>
 
+    // <div className="app">
+    //   <Board />
+    //   </div>
+
+    // <div className="app">
+    //   <div>
+    //     <button
+    //       onClick={event => {
+    //         console.log('hello', event);
+    //       }}
+    //     >
+    //       click me
+    //     </button>
+    //   </div>
+    // </div>
+
     <div className="app">
-      <Board />
+      <div>
+        <button onClick={onBtnClick}>click me</button>
+        <div>{counter}</div>
+      </div>
     </div>
   );
 }
